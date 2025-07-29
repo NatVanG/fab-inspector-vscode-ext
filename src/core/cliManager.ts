b195bf85c315d0f9365cacbc23a3060ba256c9e9
Nat Van Gulck <nathaniel.vangulck@microsoft.com>
2025-07-29 17:43:12 +0100

    Fixed issue with AdmZip dependency.

--- src/core/cliManager.ts
+++ src/core/cliManager.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as https from 'https';
-import AdmZip = require('adm-zip');
+import AdmZip from 'adm-zip';
 
 export class CliManager {
     private readonly extensionPath: string;
